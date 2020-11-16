1a39528281bf3771a5bf6e9ebc7bcbe1711dd529
Hedwig <hedwigklamer@hotmail.com>
2020-11-16 15:28:50 +0100

    Break Glow

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-// function myTimer() {
-//     var downloadTimer = setInterval(function() {
-//         document.getElementById("myPopup").innerHTML = `${timeleft} seconds left`;
-//         timeleft -= 1;
-//         if (timeleft <= 0) {
-//             console.log('time has runned out');
-//             clearInterval(downloadTimer);
-//             document.getElementById("myPopup").innerHTML = "The time has ended!";
-//             document.getElementById("dot").style.backgroundColor = "#6495ED" ;
-//             document.getElementById("dot").style.boxShadow = "1px 1px 15px rgba(227, 238, 255 ,1)";
-//         }
-//     }, 1000);
-// }
-
-// function resetTimer() {
-//     console.log('reset timer activated');
-//     timeleft = 5;
-//     document.getElementById("dot").style.backgroundColor = "#fcba03" ;
-//     document.getElementById("dot").style.boxShadow = "1px 1px 15px rgba(255,160,0 ,1)";
-//     myTimer();
-// }
-
 document.getElementById('myPopup').innerHTML = 000 + ":" + 10;
 startTimer();
 var time; //Store timer in var so you can stop starting multiple timers
@@ -29,11 +7,15 @@ function startTimer() {
     var timeArray = presentTime.split(/[:]+/);
     var m = timeArray[0];
     var s = checkSecond((timeArray[1] - 1));
-    if(s==59){m=m-1}  
+    if(s==59){
+        m=m-1
+    }  
     if(m<0){
-        document.getElementById("myPopup").innerHTML = "The time has ended!";
+        document.getElementById("myPopup").innerHTML = "time for a break";
         document.getElementById("dot").style.backgroundColor = "#6495ED" ;
-        document.getElementById("dot").style.boxShadow = "1px 1px 15px rgba(227, 238, 255 ,1)"
+        document.getElementById("dot").style.boxShadow = "1px 1px 15px rgba(227, 238, 255 ,1)";
+        document.getElementById("dot").classList.remove("dotAni");
+        document.getElementById("dot").classList.add("glow")
         exit()
     } else{
         document.getElementById('myPopup').innerHTML =m + ":" + s;
@@ -43,8 +25,12 @@ function startTimer() {
 }
 
 function checkSecond(sec) {
-    if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
-    if (sec < 0) {sec = "59"};
+    if (sec < 10 && sec >= 0) {
+        sec = "0" + sec
+    }; // add zero in front of numbers < 10
+    if (sec < 0) {
+        sec = "59"
+    };
     return sec;
   }
 
@@ -54,6 +40,8 @@ function resetTimer() {
     document.getElementById('myPopup').innerHTML = 000 + ":" + 10;
     document.getElementById("dot").style.backgroundColor = "#fcba03" ;
     document.getElementById("dot").style.boxShadow = "1px 1px 15px rgba(255,160,0 ,1)";
+    document.getElementById("dot").classList.remove("glow");
+        document.getElementById("dot").classList.add("dotAni")
     startTimer();
 }
 
@@ -62,5 +50,3 @@ function hoverPopup() {
     popup.classList.toggle("show");
   }
 
-// var timeleft = 80;
-// myTimer();
